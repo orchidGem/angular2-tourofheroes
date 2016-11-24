cbf6bd0025d649b1d273b4e18d40a8b91d68e6f3
Laura Evans <lauraevans@Lauras-MacBook-Pro-2.local>
2016-11-24 12:49:07 +0200

    add selectedHero property and click event, edit selected hero ngModel from hero to selectedHero, show selectedHero container only when selectedHero is chosen

--- app/app.component.ts
+++ app/app.component.ts
@@ -28,19 +28,21 @@ const HEROES: Hero[] = [
     <!-- Display list of Heroes-->
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span>{{hero.name}}
       </li>
     </ul>
 
-    <!-- Display Hero Details -->
-    <h2>{{hero.name}} details!</h2>
-    <div>
-      <label>id:</label>{{hero.id}}
-    </div>
-    <div>
-      <label>name:</label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+    <!-- Display Selected Hero Details -->
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div>
+        <label>id:</label>{{selectedHero.id}}
+      </div>
+      <div>
+        <label>name:</label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name">
+      </div>
     </div>
   `,
   styles: [`
@@ -95,7 +97,10 @@ const HEROES: Hero[] = [
 })
 export class AppComponent  {
   title = 'Tour of Heroes';
-  hero : Hero = { id: 1, name: "Windstorm" };
-
+  selectedHero: Hero;
   heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
