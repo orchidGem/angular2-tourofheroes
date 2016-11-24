d99047e59f2e3aa4a3e50f9e1ee8b75207d979c6
Laura Evans <lauraevans@Lauras-MacBook-Pro-2.local>
2016-11-24 16:14:38 +0200

    fix heroes service on dashboard, fix template module id path

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -4,17 +4,19 @@ import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
-  moduleId: module.id,
+  moduleId: module.id.replace("/dist/", "/"),
   selector: 'my-dashboard',
   templateUrl: 'dashboard.component.html',
 })
 
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
+
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService)
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1,5));
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
