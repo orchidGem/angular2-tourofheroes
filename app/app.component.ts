4d30db7b9bceb4b5197cd0280e9c02f7d6e7070a
Laura Evans <lauraevans@Lauras-MacBook-Pro-2.local>
2016-11-24 17:42:29 +0200

    move styles and templates to their own files

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 @Component({
+  moduleId: module.id.replace("/dist/", "/"),
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
     <nav>
-      <a routerLink="/heroes">Heroes</a>
-      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
     </nav>
     <router-outlet></router-outlet>
-  `
+  `,
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
