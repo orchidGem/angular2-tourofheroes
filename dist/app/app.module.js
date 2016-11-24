80322e609b88a71e4b93e1b4f6f731a584a07193
Laura Evans <lauraevans@Lauras-MacBook-Pro-2.local>
2016-11-24 13:14:42 +0200

    move hero detail and hero class to their own files, add the new Hero Detail Component to the App Module

--- dist/app/app.module.js
+++ dist/app/app.module.js
@@ -11,14 +11,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
+var hero_detail_component_1 = require('./hero-detail.component');
 var app_component_1 = require('./app.component');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
-            declarations: [app_component_1.AppComponent],
+            imports: [
+                platform_browser_1.BrowserModule,
+                forms_1.FormsModule,
+            ],
+            declarations: [app_component_1.AppComponent, hero_detail_component_1.HeroDetailComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
