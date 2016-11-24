e5b00cdc16cf56482424be0386a2ae686e9b688f
Laura Evans <lauraevans@Lauras-MacBook-Pro-2.local>
2016-11-24 12:20:40 +0200

    add list of heroes array

--- dist/app/app.component.js
+++ dist/app/app.component.js
@@ -15,6 +15,18 @@ var Hero = (function () {
     return Hero;
 }());
 exports.Hero = Hero;
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
